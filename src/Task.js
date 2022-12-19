adf8cd270193ae9df90ff2e20b6d696d0e0d0ea0
Istifar <istifarriyanti27@students.unnes.ac.id>
2022-12-19 12:59:43 +0700

    second commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Istifar Riyanti" // TODO: replace this
+const studentId = "FE2768094" // TODO: replace this
+
+export { url, studentName, studentId }
